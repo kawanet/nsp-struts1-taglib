395ec05049827c1b4a2d3a91943d6c83cf9c0b8e
Toshiyuki Saito <toshiyuki.saito.16@gmail.com>
2023-09-05 11:27:07 +0900

    <html:select>, <html:option>

--- src/util/BeanUtilsBean.ts
+++ src/util/BeanUtilsBean.ts
@@ -1,3 +1,4 @@
+import { ConvertUtilsBean } from "./ConvertUtilsBean.js";
 import {PropertyUtilsBean} from "./PropertyUtilsBean.js";
 
 export class BeanUtilsBean {
@@ -11,5 +12,31 @@ export class BeanUtilsBean {
 
     getPropertyUtils() {
         return this.propertyUtilsBean ??= new PropertyUtilsBean();
+    }
+
+    convertUtilsBean: ConvertUtilsBean;
+
+    public getConvertUtils(): ConvertUtilsBean {
+        return this.convertUtilsBean ??= new ConvertUtilsBean();
+    }
+
+    getArrayProperty(bean: any, name: string): any[] {
+        const value = this.getPropertyUtils().getProperty(bean, name);
+        if (value == null) {
+            return null;
+        } else if (Array.isArray(value)) {
+            const values = [];
+
+            for (const item of value) {
+                if (item == null) {
+                    values.push(null);
+                } else {
+                    values.push(this.getConvertUtils().convert(item));
+                }
+            }
+        } else {
+            const results = [this.getConvertUtils().convert(value)];
+            return results;
+        }
     }
 }
---
+++ src/util/ConvertUtilsBean.ts
@@ -0,0 +1,37 @@
+interface Converter {
+    convert (value: any): any;
+}
+
+export class ConvertUtilsBean {
+
+    private converters: {[key: string]: Converter} = {
+        string: {convert: (value: any) => `${value}`},
+    };
+
+    public lookup(clazz: string): Converter {
+        return this.converters[clazz];
+    }
+
+    public convert(value: any): string {
+        if (value == null) {
+            return null;
+        } else {
+            if (Array.isArray(value)) {
+                if (value.length < 1) {
+                    return null;
+                } else {
+                    value = value[0];
+                    if (value == null) {
+                        return null;
+                    } else {
+                        const converter = this.lookup("string");
+                        return converter.convert(value);
+                    }
+                }
+            } else {
+                const converter = this.lookup("string");
+                return converter.convert(value);
+            }
+        }
+    }
+}
