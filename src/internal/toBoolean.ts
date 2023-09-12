91d2e822e4200f2a9370812895f4b02b61ca5dd4
Yusuke Kawasaki <u-suke@kawa.net>
2023-09-12 23:48:16 +0900

    toBoolean simplified

--- src/internal/toBoolean.ts
+++ src/internal/toBoolean.ts
@@ -1,8 +1,4 @@
 /**
  * Converts a value to a boolean.
  */
-export const toBoolean = (value: any): boolean =>
-    (value == null || value === "") ? null :
-        ("boolean" === typeof value) ? value :
-            ("true" === value || 1 === +value) ? true :
-                ("false" === value || 0 === +value) ? false : !!value;
+export const toBoolean = (value: any): boolean => (value == null || value === "") ? null : ("false" === value) ? false : !!value;
